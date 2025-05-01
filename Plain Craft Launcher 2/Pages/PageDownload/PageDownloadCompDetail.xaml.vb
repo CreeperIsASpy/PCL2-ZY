5b8495e2dade5376964eb2cc1d63bc37f4a514cb
任天天 <ruattqwq@gmail.com>
2025-05-01 21:51:21 +0800

    improve(UI): 更紧凑的 Mod 详情页按钮

--- Plain Craft Launcher 2/Pages/PageDownload/PageDownloadCompDetail.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageDownload/PageDownloadCompDetail.xaml.vb
@@ -243,7 +243,7 @@
         PanIntro.Children.Insert(0, CompItem)
 
         '决定按钮显示
-        BtnIntroWeb.Text = If(Project.FromCurseForge, "转到 CurseForge", "转到 Modrinth")
+        BtnIntroWeb.Text = If(Project.FromCurseForge, "CurseForge", "Modrinth")
         BtnIntroWiki.Visibility = If(Project.WikiId = 0, Visibility.Collapsed, Visibility.Visible)
 
         AniControlEnabled -= 1
